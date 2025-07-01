56ae83a4696ae773dc45044932ae06e8600b092a
hootbee <xx3xx27@gmail.com>
2025-07-01 17:23:52 +0900

    feat: PromptController 추가

---
+++ backend/src/main/java/com/example/oda/controller/PromptController.java
@@ -0,0 +1,30 @@
+package com.example.oda.controller;
+
+import com.example.oda.dto.PromptRequestDto;
+import com.example.oda.service.PromptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PromptController {
+
+    private final PromptService promptService;
+
+    // 생성자를 통한 의존성 주입 (권장 방식)
+    @Autowired
+    public PromptController(PromptService promptService) {
+        this.promptService = promptService;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/api/prompt")
+    public ResponseEntity<String> handlePrompt(@RequestBody PromptRequestDto requestDto) {
+        String prompt = requestDto.getPrompt();
+        String response = promptService.processPrompt(prompt);
+        return ResponseEntity.ok(response);
+    }
+}
