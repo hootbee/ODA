70aa31b5957dbf8929121938bf2e92588fe067be
hyunjiiikim <hjk385900@gmail.com>
2025-08-14 15:35:37 +0900

    fix: 메시지 전송창 수정

--- frontend/src/components/MessageForm.js
+++ frontend/src/components/MessageForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'; // styled-components import 추가
+import { FiArrowUp } from "react-icons/fi";
 
 function MessageForm({ inputValue, setInputValue, handleSendMessage }) {
     return (
@@ -10,7 +11,9 @@ function MessageForm({ inputValue, setInputValue, handleSendMessage }) {
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
             />
-            <SendButton type="submit">전송</SendButton>
+            <SendButton type="submit">
+                <FiArrowUp />
+            </SendButton>
         </MessageFormContainer>
     );
 }
@@ -19,34 +22,44 @@ function MessageForm({ inputValue, setInputValue, handleSendMessage }) {
 
 const MessageFormContainer = styled.form`
   display: flex;
-  padding: 10px;
-  border-top: 1px solid #ccc;
+  align-items: center;
+  padding: 1rem 1.5rem;
+  border-top: 1px solid #e0e9ff;
+  background-color: #ffffff;
 `;
 
 const MessageInput = styled.input`
   flex-grow: 1;
-  border: 1px solid #ccc;
-  border-radius: 18px;
-  padding: 10px 15px;
+  border: 1px solid #d1e0f0;
+  border-radius: 50px;
+  padding: 12px 20px;
   font-size: 16px;
+  transition: all 0.3s ease;
+
   &:focus {
     outline: none;
-    border-color: #007bff;
+    border-color: #81cdffff;
+    box-shadow: 0 0 0 2px rgba(59, 130, 246, 0.2);
   }
 `;
 
 const SendButton = styled.button`
-  background-color: #007bff;
+  width: 44px;
+  height: 44px;
+  background-color: #0099ffff;
   color: white;
   border: none;
-  border-radius: 18px;
-  padding: 10px 20px;
+  border-radius: 50%;
   margin-left: 10px;
   cursor: pointer;
-  font-size: 16px;
+  font-size: 23px;
   transition: background-color 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   &:hover {
-    background-color: #0056b3;
+    background-color: #0066aaff;
   }
 `;
 
