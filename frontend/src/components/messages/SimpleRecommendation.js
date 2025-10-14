5d6576389961ec52f3f82b8e9292e368046f7a3c
hyunjiiikim <hjk385900@gmail.com>
2025-10-15 01:08:15 +0900

    fix: 메인 도움말 수정

--- frontend/src/components/messages/SimpleRecommendation.js
+++ frontend/src/components/messages/SimpleRecommendation.js
@@ -83,7 +83,7 @@ export default SimpleRecommendation;
 const RecommendationList = styled.div`
     background: white;
     border-radius: 20px;
-    padding: 3px;
+    padding: 20px;
     border: 1px solid #e2e8f0;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     display: flex;
